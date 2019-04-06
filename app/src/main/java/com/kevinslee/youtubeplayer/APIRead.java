5be3a859b3ca2955fe7608aca37d48419e8f59cc
KSLeeProjects <supermeetboi11@gmail.com>
2019-04-06 12:15:06 -0700

    Working information

---
+++ app/src/main/java/com/kevinslee/youtubeplayer/APIRead.java
@@ -0,0 +1,6 @@
+package com.kevinslee.youtubeplayer;
+
+public class APIRead {
+    //The API key must be retrieve from Google and added to our sacred folder
+    String API_KEY = BuildConfig.MY_API_KEY;
+}
